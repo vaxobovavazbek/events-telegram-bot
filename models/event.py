30b93073afba13900e55dfc39a26b043be0f80c7
oriash93 <oriash93@gmail.com>
2021-12-04 20:51:25 +0200

    Better event notification format

--- models/event.py
+++ models/event.py
@@ -1,14 +1,12 @@
 from datetime import datetime
 
 from bot.i18n_utils import translate as _
-from database import venues_database as venues
 
 
 class Event:
     def __init__(self, description: str, venue_id: str, event_time: str):
         self.description = description
         self.venue_id = venue_id
-        self.venue_name = venues.retrieve_display_name_by_venue_id(self.venue_id)
         self.event_time = datetime.fromisoformat(event_time)
 
     @classmethod
@@ -17,4 +15,4 @@ class Event:
 
     def __str__(self) -> str:
         return _('EVENT_NOTIFICATION_FORMAT',
-                 venue=_(self.venue_name), description=self.description, time=self.event_time.strftime("%H:%M"))
+                 venue=_(self.venue_id), description=self.description, time=self.event_time.strftime("%H:%M"))
